6eb5852689356fc75fedaf4c46d8f5021eb85707
kuasi <adrian11gomez@gmail.com>
2016-03-11 23:14:36 +0100

    add mongo logic for one sport

--- server/app.js
+++ server/app.js
@@ -10,6 +10,9 @@ app.use( express.static(__dirname + "/../client") );
 app.get("/sports", (request, response) => {
   let sports = mongoUtil.sports();
   sports.find().toArray( (err, docs) => {
+    if (err) {
+      reponse.sendStatus(400);
+    }
     const sportsNames = docs.map(sport => sport.name);
     response.json(sportsNames);
   });
@@ -17,20 +20,14 @@ app.get("/sports", (request, response) => {
 
 app.get("/sports/:name", (request, response) => {
   const sportName = request.params.name;
-  console.log("Name sport: ", sportName);
-  const sport = {
-    "name": "Cycling",
-    "goldMedals": [{
-      "division": "Men's Sprint",
-      "country": "UK",
-      "year": 2012
-    }, {
-      "division": "Women's Sprint",
-      "country": "Australia",
-      "year": 2012
-    }]
-  };
-  response.json(sport);
+  let sports = mongoUtil.sports();
+  sports.find({name: sportName}).limit(1).next((err, doc) => {
+    if (err) {
+      reponse.sendStatus(400);
+    }
+    console.log("Name sport: ", doc);
+    response.json(doc);
+  });
 });
 
 app.listen(8888, () => console.log( "Listening on 8888" ));
